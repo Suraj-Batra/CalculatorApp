55697cc18bca1c6fe50ae838fe1d95c250136640
Suraj Batra <surajbatra240@gmail.com>
2020-12-23 20:51:30 +0530

    Calculator app running successfuly

--- www/js/index.js
+++ www/js/index.js
@@ -1,7 +1,10 @@
  document.addEventListener('deviceready', onDeviceReady, false);
 
-function onDeviceReady() {
-
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
-}
+ function onDeviceReady() {
+        document.getElementById('addbutton').addEventListener('click', Add);
+           function Add(){
+           var num1 = document.getElementById("firstNumber").value;
+           var num2 = document.getElementById("SecondNumber").value;
+           document.getElementById("addresult").innerHTML = "Result : " + (Number(num1) + Number(num2));
+        }
+ }
