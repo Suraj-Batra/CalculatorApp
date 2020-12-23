9a07af5a5daf0b07578bdd5ac1338b07cf410825
Suraj Batra <surajbatra240@gmail.com>
2020-12-23 09:01:43 +0530

    Adding changes in index.html file

--- www/js/main.js
+++
@@ -1,7 +0,0 @@
-function add_number(){
-     var first_number = parseInt(document.getElementById("Text1").value);
-     var second_number = parseInt(document.getElementById("Text2").value);
-     var result = 'first_number' + 'second_number';
-
-    document.getElementById("Result").innerHTML = 'result';
-}
