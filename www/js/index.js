8c21996844ea08f465693cf5d4c2a5d267b3592b
Suraj Batra <surajbatra240@gmail.com>
2020-12-24 15:58:40 +0530

    New changes Added in index.html and index.js

--- www/js/index.js
+++ www/js/index.js
@@ -5,6 +5,24 @@
            function Add(){
            var num1 = document.getElementById("firstNumber").value;
            var num2 = document.getElementById("SecondNumber").value;
-           document.getElementById("addresult").innerHTML = "Result : " + (Number(num1) + Number(num2));
+           document.getElementById("result").innerHTML = "Result : " + (Number(num1) + Number(num2));
         }
- }
+         document.getElementById('subbutton').addEventListener('click', Sub);
+            function Sub(){
+            var num1 = document.getElementById("firstNumber").value;
+            var num2 = document.getElementById("SecondNumber").value;
+            document.getElementById("result").innerHTML = "Result : " + (Number(num1) - Number(num2));
+         }
+         document.getElementById('multibutton').addEventListener('click', multiply);
+         function multiply(){
+         var num1 = document.getElementById("firstNumber").value;
+         var num2 = document.getElementById("SecondNumber").value;
+         document.getElementById("result").innerHTML = "Result : " + (Number(num1) * Number(num2));
+      }
+      document.getElementById('rembutton').addEventListener('click', remainder);
+      function remainder(){
+      var num1 = document.getElementById("firstNumber").value;
+      var num2 = document.getElementById("SecondNumber").value;
+      document.getElementById("result").innerHTML = "Result : " + (Number(num1) / Number(num2));
+   }
+}
